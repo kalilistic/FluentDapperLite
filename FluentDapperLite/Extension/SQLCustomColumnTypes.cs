8ac9e58b193f1d9d937d79a4f12cd82d9c40f9b2
kalilistic <35899782+kalilistic@users.noreply.github.com>
2023-11-12 13:37:00 -0500

    feat: add custom types support for alter

--- FluentDapperLite/Extension/SQLCustomColumnTypes.cs
+++ FluentDapperLite/Extension/SQLCustomColumnTypes.cs
@@ -1,12 +1,13 @@
 ﻿namespace FluentDapperLite.Extension;
 
+using FluentMigrator.Builders.Alter.Table;
 using FluentMigrator.Builders.Create.Table;
 
 /// <summary>
-/// Provides custom column type extensions for FluentMigrator's table column creation syntax.
+/// Provides custom column type extensions for FluentMigrator's table column creation and alteration syntax.
 /// </summary>
 /// <remarks>
-/// This class extends FluentMigrator's ICreateTableColumnAsTypeSyntax to add support for unsigned integer column types.
+/// This class extends FluentMigrator's ICreateTableColumnAsTypeSyntax and IAlterTableColumnAsTypeSyntax to add support for unsigned integer column types.
 /// </remarks>
 public static class SQLCustomColumnTypes
 {
@@ -34,4 +35,28 @@ public static class SQLCustomColumnTypes
     /// <param name="columnName">The name of the column.</param>
     /// <returns>An ICreateTableColumnOptionOrWithColumnSyntax instance with the added constraint.</returns>
     public static ICreateTableColumnOptionOrWithColumnSyntax AsUInt64(this ICreateTableColumnAsTypeSyntax column, string columnName) => column.AsCustom($"INTEGER CHECK({columnName} >= 0)");
+
+    /// <summary>
+    /// Adds or alters a column of type SMALLINT with a constraint to mimic UInt16 in an existing table.
+    /// </summary>
+    /// <param name="column">The IAlterTableColumnAsTypeSyntax instance to extend.</param>
+    /// <param name="columnName">The name of the column.</param>
+    /// <returns>An IAlterTableColumnOptionOrAddColumnOrAlterColumnSyntax instance with the added constraint.</returns>
+    public static IAlterTableColumnOptionOrAddColumnOrAlterColumnSyntax AsUInt16(this IAlterTableColumnAsTypeSyntax column, string columnName) => column.AsCustom($"SMALLINT CHECK({columnName} >= 0 AND {columnName} <= 65535)");
+
+    /// <summary>
+    /// Adds or alters a column of type INTEGER with a constraint to mimic UInt32 in an existing table.
+    /// </summary>
+    /// <param name="column">The IAlterTableColumnAsTypeSyntax instance to extend.</param>
+    /// <param name="columnName">The name of the column.</param>
+    /// <returns>An IAlterTableColumnOptionOrAddColumnOrAlterColumnSyntax instance with the added constraint.</returns>
+    public static IAlterTableColumnOptionOrAddColumnOrAlterColumnSyntax AsUInt32(this IAlterTableColumnAsTypeSyntax column, string columnName) => column.AsCustom($"INTEGER CHECK({columnName} >= 0 AND {columnName} <= 4294967295)");
+
+    /// <summary>
+    /// Adds or alters a column of type INTEGER with a constraint to mimic UInt64 in an existing table.
+    /// </summary>
+    /// <param name="column">The IAlterTableColumnAsTypeSyntax instance to extend.</param>
+    /// <param name="columnName">The name of the column.</param>
+    /// <returns>An IAlterTableColumnOptionOrAddColumnOrAlterColumnSyntax instance with the added constraint.</returns>
+    public static IAlterTableColumnOptionOrAddColumnOrAlterColumnSyntax AsUInt64(this IAlterTableColumnAsTypeSyntax column, string columnName) => column.AsCustom($"INTEGER CHECK({columnName} >= 0)");
 }
